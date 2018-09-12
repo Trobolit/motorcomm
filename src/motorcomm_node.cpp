97423101547cae2bc0c78090cf7e7bd2fa33abfd
Robert Hedman <robert.hedman@mac.com>
2018-09-12 11:04:42 +0200

    reads Twist message with example

--- src/motorcomm_node.cpp
+++ src/motorcomm_node.cpp
@@ -1,12 +1,38 @@
 #include "ros/ros.h"
-#include "std_msgs/Byte.h"
+#include "geometry_msgs/Twist.h"
 
 /**
  * This tutorial demonstrates simple receipt of messages over the ROS system.
  */
-void motorSpeedCallback(const std_msgs::Byte msg)
+
+geometry_msgs::Twist vel_msg;
+
+void motorPowerCallback(const geometry_msgs::Twist::ConstPtr & msg)
 {
-  ROS_INFO("I got byte: %d! :D", msg);
+	vel_msg = *msg; // This is how one can modify the data coming in.
+	vel_msg.linear.x = 666;
+
+	// Example on how to access the data, both from msg and vel_msg. Note the arrow.
+	// It seems msg->anglular.z is essentially (*msg).angular.z
+	// Note: Using the arrow the data is read only, which is normally what you need.
+	// Using the pointers this way the data is never copied around, which is good for 
+	// performance on the pi!
+	ROS_INFO("I got this:\n Linear:\nx:%f\ny:%f\nz:%f\n MegaAngle:\nx:%f\ny:%f\nz:%f\n",
+		vel_msg.linear.x, vel_msg.linear.y,msg->linear.z,
+		msg->angular.x, msg->angular.y, msg->angular.z);
+
+
+/* To test in ros, first run this node, then issue the command:
+* rostopic pub /motor_power geometry_msgs/Twist "linear:
+  x: 1.0
+  y: 2.0
+  z: 3.0
+angular:
+  x: 4.0
+  y: 5.0
+  z: 1.0" -1
+
+*/
 }
 
 int main(int argc, char **argv)
@@ -45,7 +71,7 @@ int main(int argc, char **argv)
    * is the number of messages that will be buffered up before beginning to throw
    * away the oldest ones.
    */
-  ros::Subscriber sub = n.subscribe<std_msgs::Byte>("MotorSpeed", 200, motorSpeedCallback);
+  ros::Subscriber sub = n.subscribe<geometry_msgs::Twist>("motor_power", 200, motorPowerCallback);
 
   /**
    * ros::spin() will enter a loop, pumping callbacks.  With this version, all
